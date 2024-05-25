aa6c0555bfb32ecf4669ff7edea23c1eb317c7c0
theboyGolden <preciousstick15@gmail.com>
2024-05-25 20:11:34 +0000

    I chnaged the text in the app.js file to mention my name

--- Myapp/App.js
+++ Myapp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is Amenuveve Precious Tsewoo</Text>
       <StatusBar style="auto" />
     </View>
   );
